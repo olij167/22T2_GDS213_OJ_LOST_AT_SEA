818a7a1f9fc771e8d17d12f974db655663da0bf5
Oli <1024958@student.sae.edu.au>
2022-08-25 13:04:02 +1000

    Fix boat flying, break boat steering
    lol

--- Assets/Scripts/Motor.cs
+++ Assets/Scripts/Motor.cs
@@ -23,14 +23,40 @@ public class Motor : MonoBehaviour
         boat.transform.position += boat.transform.forward * speed * Time.deltaTime;
 
         //// steer
+        float steerAngle = boat.transform.rotation.eulerAngles.y - stickPivot.rotation.eulerAngles.y;
+        float xCorrectAngle = -boat.transform.rotation.eulerAngles.x;
+        float zCorrectAngle = -boat.transform.rotation.eulerAngles.z;
+        boat.transform.Rotate(xCorrectAngle * Time.deltaTime, steerAngle * Time.deltaTime, zCorrectAngle * Time.deltaTime);
 
         //stickRotation.y = (stickRotation.y > 180) ? stickRotation.y - 360 : stickRotation.y;
 
-        Vector3 stickPivotXZDirection = new Vector3(stickPivot.transform.right.x, 0, 0);
+        //Vector3 stickPivotXZDirection = new Vector3(stickPivot.transform.position.x, 0, stickPivot.transform.position.z);
 
-        Vector3 boatButLower = new Vector3(boat.transform.position.x, boat.transform.position.y - forceDistanceFromBoat, boat.transform.position.z);
+        //Vector3 boatNose = new Vector3(boat.transform.position.x + forceDistanceFromBoat, boat.transform.position.y, boat.transform.position.z);
 
-        boat.AddForceAtPosition(stickPivotXZDirection * rotationSpeed * Time.deltaTime, transform.position, force);
+        //boat.AddForceAtPosition(speed * stickPivotXZDirection, boatNose, force);
+        //boat.transform.rotation = stickPivotXZDirection * rotationSpeed * Time.deltaTime;
+        // Smoothly tilts a transform towards a target rotation.
+        //float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        //float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        // Rotate the cube by converting the angles into a quaternion.
+        //Quaternion target = Quaternion.Euler(0, stickPivot.rotation.y, 0);
+
+        // Dampen towards the target rotation
+        //transform.rotation = Quaternion.Slerp(boat.transform.rotation, target, Time.deltaTime * rotationSpeed);
+
+
+        //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+
+        //x += Time.deltaTime * 10;
+        //transform.rotation = Quaternion.Euler(x, 0, 0);
+
+        //float turnAngle = boat.transform.rotation.eulerAngles.y - stickPivot.transform.eulerAngles.y;
+        //Vector3 eulerAngleVelocity = new Vector3(0, 0, 0);
+        //boat.transform.rotation = Quaternion.Euler(0,turnAngle * Time.deltaTime,0);
+        
+     
 
     }
 }
